db7b71cb3b629425084ab58f40e1ef1ac903e8f8
Hans Oliveira <hansnery@gmail.com>
2021-08-02 16:35:33 -0300

    Inital commit

---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  get '/privacy', to: 'home#privacy'
+  get '/terms', to: 'home#terms'
+authenticate :user, lambda { |u| u.admin? } do
+  mount Sidekiq::Web => '/sidekiq'
+
+  namespace :madmin do
+    namespace :active_storage do
+      resources :variant_records
+    end
+    namespace :active_storage do
+      resources :attachments
+    end
+    namespace :active_storage do
+      resources :blobs
+    end
+    resources :users
+    resources :services
+    resources :notifications
+    resources :announcements
+    root to: "dashboard#show"
+  end
+end
+
+  resources :notifications, only: [:index]
+  resources :announcements, only: [:index]
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  root to: 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
