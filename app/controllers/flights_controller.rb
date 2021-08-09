567729de0865a8def189cc14fa4e6009e24b51ca
Hans Oliveira <hansnery@gmail.com>
2021-08-09 17:13:01 -0300

    Can search flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    @select_dates = Flight.all.map { |flight| [flight.datetime.strftime("%Y/%m/%d"), flight.datetime.strftime("%Y/%m/%d")] }
+    @select_dates = Flight.all.map { |flight| [flight.datetime.strftime("%Y/%m/%d"), flight.datetime] }
     @airports = Airport.all
-    @select_airports = Airport.all.map { |airport| [airport.city, airport.airport_code, airport.id] }
+    @select_airports = Airport.all.map { |airport| [airport.city, airport.id] }
   end
 end
