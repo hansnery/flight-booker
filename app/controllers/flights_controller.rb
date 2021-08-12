d4e9049db2e67f342f043ea1afc6ff0c46d9f165
Hans Oliveira <hansnery@gmail.com>
2021-08-12 17:42:52 -0300

    Add warning to blank form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,17 @@ class FlightsController < ApplicationController
     @select_dates = Flight.all.map { |flight| [flight.datetime.strftime("%Y/%m/%d"), flight.datetime] }
     @airports = Airport.all
     @select_airports = Airport.all.map { |airport| [airport.city, airport.id] }
+    if @search.present?
+      @search.values.map do |n|
+        flash.now[:alert] = "Invalid search!" if n.blank?
+        return
+      end
+    end
   end
+
+  # private
+
+  # def flight_params
+  #   params.require(:flight).permit(:from_id, :to_id, :datetime, :flight_duration)
+  # end
 end
